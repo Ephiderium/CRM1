7c41246567ce4100aca11e74cf1b85244066812a
Ephiderium <winnerviking@gmail.com>
2026-02-12 13:03:35 +0200

    feat(excel): add import from Excel

--- app/Exсel/ExportLog.php
+++ app/Exсel/ExportLog.php
@@ -10,10 +10,53 @@ class ExportLog implements FromCollection
     public function collection()
     {
         $log = DB::table('audit_logs')
-            ->select('*')
+            ->select([
+                'id',
+                'user_id',
+                'action',
+                'entity_type',
+                'entity_id',
+                'old_values',
+                'new_values',
+                'created_at',
+
+            ])
+            ->orderBy('created_at', 'desc')
             ->limit(1000)
             ->get();
 
         return $log;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Событие',
+            'Объект',
+            'ID объекта',
+            'Пользователь',
+            'Дата и время'
+        ];
+    }
+
+    public function map($row): array
+    {
+        $modelName = match($row->entity_type) {
+            'App\Models\User' => 'Пользователь',
+            'App\Models\Client' => 'Клиент',
+            'App\Models\Deal' => 'Сделка',
+            'App\Models\Task' => 'Задача',
+            default => class_basename($row->entity_type)
+        };
+
+        return [
+            $row->id,
+            $row->user_id,
+            $row->action,
+            $row->$modelName,
+            $row->entity_id,
+            date('d.m.Y H:i', strtotime($row->created_at))
+        ];
     }
 }
